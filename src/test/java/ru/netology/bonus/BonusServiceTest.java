ced9b6dd362536e260cd7b5fd483e9c29536cbcc
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-02-03 20:32:50 +0300

    bonus calc service added

---
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.bonus;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    " registered user, bonus under limit/ 100060/ true/ 30",
+                    "registered user, bonus over limit/ 100000060/ true/ 500"
+            },
+            delimiter = '/'
+    )
+    void shouldCalculate(String test, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+
+    }
+
+
+}
